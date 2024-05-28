879d8834ce7c118e01ee1047db925c4e27935922
shahnoorgit <shahnoormujawar@gmail.com>
2024-05-28 20:21:39 +0530

    title and subtitle done

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
 import React from "react";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <div>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+    </div>
+  );
 };
 
 export default AddTransformationTypePage;
